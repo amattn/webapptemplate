8b69725b8a45b6f14c398b7cda39de53d08cdc87
amattn <hidden@example.com>
2016-04-17 08:43:33 -0700

    update build script to be a bit kinder in the case of failing gox commands

--- build.sh
+++ build.sh
@@ -13,4 +13,12 @@ go test
 # build for our test or deployment platforms
 # normally we do local development on darwin/amd64 and deploy to linux/amd64.
 # feel free to add or remove if your situation differs
-gox -osarch="darwin/amd64" -osarch="linux/amd64"
+# also, in the normal case, most of the output of gox is redundant with
+# the output from go build above, so in the normal case, we just 
+# redirect to a build log
+GOX_BUILD_LOG="gox_build.log"
+date >> $GOX_BUILD_LOG
+if ! gox -osarch="darwin/amd64" -osarch="linux/amd64" >> $GOX_BUILD_LOG 2>&1; then
+    echo "FAILURE: gox command failed to build for deployment architecture"
+fi
+
